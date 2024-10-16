40afe0576d2d75f269c6049ef4306c9b822c910d
chaan-junior <it21831768@my.sliit.lk>
2024-10-17 00:55:19 +0530

    auth middleware update with bearer prefix and working all users

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -5,15 +5,18 @@ const JWT_SECRET = process.env.JWT_SECRET || "supersecret";
 // Middleware to verify JWT and extract user data
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
-console.log('✌️authHeader --->', authHeader);
-  if (!authHeader) {
+  console.log("✌️authHeader --->", authHeader);
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ msg: "No token, authorization denied" });
   }
 
+  // Extract the token part from the Bearer string
+  const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(authHeader, JWT_SECRET);
-    req.user = decoded; 
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
     next();
   } catch (err) {
     if (err.name === "TokenExpiredError") {
