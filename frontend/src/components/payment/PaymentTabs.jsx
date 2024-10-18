e51d8b654342f4874bb58e7034b39f2631d83be2
IT21055058 <it21055058@my.sliit.lk>
2024-10-18 22:44:22 +0530

    visual background update

--- frontend/src/components/payment/PaymentTabs.jsx
+++ frontend/src/components/payment/PaymentTabs.jsx
@@ -52,27 +52,27 @@ const PaymentTabs = () => {
             </header>
             {/* main section */}
             <div className="container mx-auto max-w-4xl p-4">
-                <div className="mb-8">
+                <div className="">
                     <h1 className="mb-2 text-2xl font-bold text-gray-800">💵 Payment Management</h1>
                     <div className="border-b border-gray-200">
-                        <nav className="flex space-x-8">
+                        <nav className="flex space-x-0">
                             <button
                                 onClick={() => setActiveTab('due')}
-                                className={`border-b-2 px-1 py-4 text-sm font-medium ${
+                                className={`border-b-2 px-2 py-4 text-sm font-medium ${
                                     activeTab === 'due'
-                                        ? 'border-green-500 text-green-600'
-                                        : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
+                                        ? 'rounded-t-md border-green-500 bg-green-300 bg-opacity-10 text-green-600'
+                                        : 'rounded-t-md border-transparent bg-slate-400 bg-opacity-10 text-gray-500 hover:border-gray-300 hover:text-gray-700'
                                 }`}
                             >
                                 Due Payments ⏱️
                             </button>
-                            <span className="mb-2 mt-2 h-10 border-x-4 border-slate-400 px-1"></span>
+                            {/* <span className="mb-2 mt-2 h-10 border-x-4 border-slate-400 px-1"></span> */}
                             <button
                                 onClick={() => setActiveTab('history')}
-                                className={`border-b-2 px-1 py-4 text-sm font-medium ${
+                                className={`border-b-2 px-2 py-4 text-sm font-medium ${
                                     activeTab === 'history'
-                                        ? 'border-green-500 text-green-600'
-                                        : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
+                                        ? 'rounded-t-md border-green-500 bg-green-300 bg-opacity-10 text-green-600'
+                                        : 'rounded-t border-transparent bg-slate-400 bg-opacity-10 text-gray-500 hover:border-gray-300 hover:text-gray-700'
                                 }`}
                             >
                                 Payment History 📜
@@ -80,8 +80,9 @@ const PaymentTabs = () => {
                         </nav>
                     </div>
                 </div>
-
-                {activeTab === 'due' ? <PendingPayments /> : <PaymentHistory />}
+                <div className="bg-green-300 bg-opacity-10 px-1 pt-4">
+                    {activeTab === 'due' ? <PendingPayments /> : <PaymentHistory />}
+                </div>
             </div>
 
             {/* Footer Section */}
