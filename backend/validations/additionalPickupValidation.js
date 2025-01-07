e5f0204b7e65c1e76c128ae0da09c5bb2d74dc7e
IT21831768 <it21831768@my.sliit.lk>
2025-01-07 14:30:00 +0530

    alteration

--- backend/validations/additionalPickupValidation.js
+++ backend/validations/additionalPickupValidation.js
@@ -2,93 +2,93 @@ const Joi = require("joi");
 const mongoose = require("mongoose");
 
 const additionalPickupValidation = {
-  createAdditionalPickup: Joi.object({
-    binId: Joi.string()
-      .custom((value, helpers) => {
-        if (!mongoose.Types.ObjectId.isValid(value)) {
-          return helpers.error("any.invalid");
-        }
-        return value;
-      })
-      .required()
-      .messages({
-        "any.invalid": "Invalid bin ID format",
-        "any.required": "Bin ID is required",
-      }),
-    wasteType: Joi.string()
-      .valid("Food", "Non Recyclable Waste", "Recyclable Waste")
-      .required(),
-    pickupDate: Joi.date().greater("now").iso().required().messages({
-      "date.greater": "Pickup date must be in the future",
-      "date.format": "Pickup date must be in ISO 8601 format",
+    createAdditionalPickup: Joi.object({
+        binId: Joi.string()
+            .custom((value, helpers) => {
+                if (!mongoose.Types.ObjectId.isValid(value)) {
+                    return helpers.error("any.invalid");
+                }
+                return value;
+            })
+            .required()
+            .messages({
+                "any.invalid": "Invalid bin ID format",
+                "any.required": "Bin ID is required",
+            }),
+        wasteType: Joi.string()
+            .valid("Food", "Non Recyclable Waste", "Recyclable Waste")
+            .required(),
+        pickupDate: Joi.date().greater("now").iso().required().messages({
+            "date.greater": "Pickup date must be in the future",
+            "date.format": "Pickup date must be in ISO 8601 format",
+        }),
+        description: Joi.string().max(500).allow("").optional(),
     }),
-    description: Joi.string().max(500).allow("").optional(),
-  }),
 
-  updatePaymentStatus: Joi.object({
-    id: Joi.string()
-      .custom((value, helpers) => {
-        if (!mongoose.Types.ObjectId.isValid(value)) {
-          return helpers.error("any.invalid");
-        }
-        return value;
-      })
-      .required()
-      .messages({
-        "any.invalid": "Invalid pickup ID format",
-        "any.required": "Pickup ID is required",
-      }),
-  }),
-
-  updatePickupStatus: Joi.object({
-    id: Joi.string()
-      .custom((value, helpers) => {
-        if (!mongoose.Types.ObjectId.isValid(value)) {
-          return helpers.error("any.invalid");
-        }
-        return value;
-      })
-      .required()
-      .messages({
-        "any.invalid": "Invalid pickup ID format",
-        "any.required": "Pickup ID is required",
-      }),
-  }),
-
-  addComplaint: Joi.object({
-    id: Joi.string()
-      .custom((value, helpers) => {
-        if (!mongoose.Types.ObjectId.isValid(value)) {
-          return helpers.error("any.invalid");
-        }
-        return value;
-      })
-      .required()
-      .messages({
-        "any.invalid": "Invalid pickup ID format",
-        "any.required": "Pickup ID is required",
-      }),
-    complaint: Joi.string().min(10).max(1000).required().messages({
-      "string.min": "Complaint must be at least 10 characters long",
-      "string.max": "Complaint must not exceed 1000 characters",
-      "any.required": "Complaint is required",
+    updatePaymentStatus: Joi.object({
+        id: Joi.string()
+            .custom((value, helpers) => {
+                if (!mongoose.Types.ObjectId.isValid(value)) {
+                    return helpers.error("any.invalid");
+                }
+                return value;
+            })
+            .required()
+            .messages({
+                "any.invalid": "Invalid pickup ID format",
+                "any.required": "Pickup ID is required",
+            }),
     }),
-  }),
 
-  getAdditionalPickupsByUserId: Joi.object({
-    userId: Joi.string()
-      .custom((value, helpers) => {
-        if (!mongoose.Types.ObjectId.isValid(value)) {
-          return helpers.error("any.invalid");
-        }
-        return value;
-      })
-      .required()
-      .messages({
-        "any.invalid": "Invalid user ID format",
-        "any.required": "User ID is required",
-      }),
-  }),
+    updatePickupStatus: Joi.object({
+        id: Joi.string()
+            .custom((value, helpers) => {
+                if (!mongoose.Types.ObjectId.isValid(value)) {
+                    return helpers.error("any.invalid");
+                }
+                return value;
+            })
+            .required()
+            .messages({
+                "any.invalid": "Invalid pickup ID format",
+                "any.required": "Pickup ID is required",
+            }),
+    }),
+
+    addComplaint: Joi.object({
+        id: Joi.string()
+            .custom((value, helpers) => {
+                if (!mongoose.Types.ObjectId.isValid(value)) {
+                    return helpers.error("any.invalid");
+                }
+                return value;
+            })
+            .required()
+            .messages({
+                "any.invalid": "Invalid pickup ID format",
+                "any.required": "Pickup ID is required",
+            }),
+        complaint: Joi.string().min(10).max(1000).required().messages({
+            "string.min": "Complaint must be at least 10 characters long",
+            "string.max": "Complaint must not exceed 1000 characters",
+            "any.required": "Complaint is required",
+        }),
+    }),
+
+    getAdditionalPickupsByUserId: Joi.object({
+        userId: Joi.string()
+            .custom((value, helpers) => {
+                if (!mongoose.Types.ObjectId.isValid(value)) {
+                    return helpers.error("any.invalid");
+                }
+                return value;
+            })
+            .required()
+            .messages({
+                "any.invalid": "Invalid user ID format",
+                "any.required": "User ID is required",
+            }),
+    }),
 };
 
 module.exports = additionalPickupValidation;
